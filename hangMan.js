05cf0161ad5936419130f20669dfa1a30537bdf9
tincho <martin.salinas@gmail.com>
2017-02-28 12:07:46 -0300

    moved Object.assign polyfill to separate file

--- hangMan.js
+++ hangMan.js
@@ -1,32 +1,11 @@
 'use strict';
-if (typeof window === 'undefined') module.exports = HangMan;
+if (typeof window === 'undefined') {
+    require("Object.assign.js");
+    module.exports = HangMan;
+}
 
 Array.prototype.clone = function() {
     return this.slice(0);
-}
-if (typeof Object.assign != 'function') {
-    Object.assign = function (target, varArgs) { // .length of function is 2
-        'use strict';
-        if (target == null) { // TypeError if undefined or null
-            throw new TypeError('Cannot convert undefined or null to object');
-        }
-
-        var to = Object(target);
-
-        for (var index = 1; index < arguments.length; index++) {
-            var nextSource = arguments[index];
-
-            if (nextSource != null) { // pasamos si es undefined o null
-                for (var nextKey in nextSource) {
-                    // Evita un error cuando 'hasOwnProperty' ha sido sobrescrito
-                    if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
-                        to[nextKey] = nextSource[nextKey];
-                    }
-                }
-            }
-        }
-        return to;
-    };
 }
 
 function HangMan(deliveranceWord) {
